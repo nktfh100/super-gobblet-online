1c24c321ddf27401f9ce8d3c4b3144eaf0525397
nktfh100 <me@nktfh100.com>
2024-06-09 11:10:42 +0300

    Remove admob

--- client/src/services/ads.js
+++ client/src/services/ads.js
@@ -1,22 +1,22 @@
-import { AdMobInterstitial, setTestDeviceIDAsync, } from 'expo-ads-admob';
-import * as Device from 'expo-device';
-import Constants from "expo-constants";
+// import { AdMobInterstitial, setTestDeviceIDAsync, } from 'expo-ads-admob';
+// import * as Device from 'expo-device';
+// import Constants from "expo-constants";
 
 export function getCorrectAdId(prodId, testId) {
-    return Device.isDevice && !__DEV__ ? prodId : testId;
+	return "";
+	// return Device.isDevice && !__DEV__ ? prodId : testId;
 }
 
 export function setupAds() {
-    AdMobInterstitial.setAdUnitID(getCorrectAdId(Constants.manifest.extra.AD_ID, "ca-app-pub-3940256099942544/6300978111"));
-    setTestDeviceIDAsync('EMULATOR');
+	// AdMobInterstitial.setAdUnitID(getCorrectAdId(Constants.manifest.extra.AD_ID, "ca-app-pub-3940256099942544/6300978111"));
+	// setTestDeviceIDAsync('EMULATOR');
 }
 
 export async function showAd() {
-    try {
-        await AdMobInterstitial.requestAdAsync();
-        await AdMobInterstitial.showAdAsync();
-    } catch (error) {
-        console.log(error);
-    }
+	// try {
+	//     await AdMobInterstitial.requestAdAsync();
+	//     await AdMobInterstitial.showAdAsync();
+	// } catch (error) {
+	//     console.log(error);
+	// }
 }
-
