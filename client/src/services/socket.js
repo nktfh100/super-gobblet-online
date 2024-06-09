b3af20662595e452a1c3f0abbd2c09c762356c27
nktfh100 <me@nktfh100.com>
2024-06-09 11:05:57 +0300

    Revert upgrade

--- client/src/services/socket.js
+++ client/src/services/socket.js
@@ -1,12 +1,10 @@
+import io from 'socket.io-client';
 import Constants from "expo-constants";
-import io from "socket.io-client";
 
 const apiUrl = __DEV__
-	? Constants.manifest && Constants.manifest.debuggerHost
-		? `http://${Constants.manifest.debuggerHost.split(":").shift()}:3000`
-		: ""
-	: "https://gobblet-server.malachi.io";
+    ? Constants.manifest && Constants.manifest.debuggerHost ? `http://${Constants.manifest.debuggerHost.split(':').shift()}:3000` : "" :
+    "https://gobblet-online.server.nktfh100.com";
 
-const socket = io(apiUrl, { transports: ["websocket"] });
+const socket = io(apiUrl, { transports: ['websocket'] });
 
 export default socket;
